f5b9bf00c9cc92e80f9f8560a993be2f34d53bb2
Jaganprafful <67191031+Jaganprafful@users.noreply.github.com>
2022-03-07 15:13:41 -0600

    Update admin.py

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Customer, Investment, Stock
+from .models import Customer, Investment, Stock, MutualFund
 
 
 class CustomerList(admin.ModelAdmin):
@@ -24,6 +24,13 @@ class StockList(admin.ModelAdmin):
     ordering = ['customer']
 
 
+class MutualFundList(admin.ModelAdmin):
+    list_display = ('customer', 'plan', 'investment_amount', 'current_value', 'acquired_date')
+    search_fields = ('customer', 'plan')
+    ordering = ['customer']
+
+
 admin.site.register(Customer, CustomerList)
 admin.site.register(Investment, InvestmentList)
 admin.site.register(Stock, StockList)
+admin.site.register(MutualFund, MutualFundList)
