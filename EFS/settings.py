414ac6e9826121874426a83378055b336061d4e4
Jaganprafful <67191031+Jaganprafful@users.noreply.github.com>
2022-03-07 00:59:18 -0600

    Update settings.py

--- EFS/settings.py
+++ EFS/settings.py
@@ -79,21 +79,21 @@ WSGI_APPLICATION = 'EFS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'dgb',
-#         'USER': 'postgres',
-#         'PASSWORD': 'gounomavs1a'
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dgb',
+        'USER': 'postgres',
+        'PASSWORD': 'gounomavs1a'
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(BASE_DIR / 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -122,7 +122,6 @@ TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
-USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
